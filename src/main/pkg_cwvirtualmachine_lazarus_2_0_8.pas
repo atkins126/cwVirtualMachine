01b51dc70fcd708b0037549d6d91fa2dfc8b30d3
chapmanworld <craig@chapmanworld.com>
2020-08-27 13:35:08 -0600

    Lazarus package and group added.

---
+++ src/main/pkg_cwvirtualmachine_lazarus_2_0_8.pas
@@ -0,0 +1,12 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit pkg_cwVirtualMachine_Lazarus_2_0_8;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+implementation
+
+end.
